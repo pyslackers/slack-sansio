2f076fafe745be22c0d8ccae5453e7f66e2bf893
Allen Anthes <abanthes@gmail.com>
2019-05-10 12:39:29 -0500

    More formatting

--- slack/tests/data/__init__.py
+++ slack/tests/data/__init__.py
@@ -1,9 +1,7 @@
 from .events import Events, Messages, RTMEvents  # noQa F401
-from .actions import (
-    MessageAction,
-    DialogSubmission,
-    InteractiveMessage,
-    BlockAction,
-)  # noQa F401
+from .actions import BlockAction  # noQa F401
+from .actions import MessageAction  # noQa F401
+from .actions import DialogSubmission  # noQa F401
+from .actions import InteractiveMessage  # noQa F401
 from .methods import Methods  # noQa F401
 from .commands import Commands  # noQa F401
